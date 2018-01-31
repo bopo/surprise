c3ae4422904fdd5dcf9ed370873dd3395f9405ba
bopo <ibopo@126.com>
2018-01-31 14:33:13 +0800

    u

--- crontab/db_backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-PGPASSWORD="secret" /usr/bin/pg_dump -U surprise surprise > /home/apps/dbs/surprise/$(date +%Y-%m-%d_%H:%M).backup
-# /root/.virtualenvs/surprise/bin/python /home/apps/surprise/manage.py dumpdata --format=json > /home/apps/surprise/database/backups/$(date +%Y-%m-%d_%H:%M).json
