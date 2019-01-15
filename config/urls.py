88b46e4ed0c2af7d2d82086206824dc808e4d00e
bopo <ibopo@126.com>
2019-01-16 00:01:34 +0800

    update commit

--- config/urls.py
+++ config/urls.py
@@ -7,24 +7,23 @@ from django.contrib import admin
 from django.contrib.flatpages.views import flatpage
 
 urlpatterns = (
-    url(r'^share/extract/(?P<slug>.*)$', 'frontend.views.share_extract', name='share_extract'),
-    url(r'^share/invite/(?P<slug>.*)$', 'frontend.views.share_invite', name='share_invite'),
-    url(r'^share/qrcode/(?P<slug>.*)$', 'frontend.views.share_qrcode', name='share_qrcode'),
-    url(r'^share/prize/(?P<slug>.*)$', 'frontend.views.share_prize', name='share_prize'),
+    url(r'^share/extract/(?P<slug>.*)$', 'service.frontend.views.share_extract', name='share_extract'),
+    url(r'^share/invite/(?P<slug>.*)$', 'service.frontend.views.share_invite', name='share_invite'),
+    url(r'^share/qrcode/(?P<slug>.*)$', 'service.frontend.views.share_qrcode', name='share_qrcode'),
+    url(r'^share/prize/(?P<slug>.*)$', 'service.frontend.views.share_prize', name='share_prize'),
 
-    url(r'^r/(?P<mark>.*)$', 'frontend.views.r', name='r'),
-    url(r'^q/(?P<uid>.*)$', 'frontend.views.q', name='q'),
+    url(r'^r/(?P<mark>.*)$', 'service.frontend.views.r', name='r'),
+    url(r'^q/(?P<uid>.*)$', 'service.frontend.views.q', name='q'),
 
-    url(r'^$', 'frontend.views.home', name='home'),
-    url(r'^help', 'frontend.views.help', name='help'),
-    url(r'^packet$', 'frontend.views.packet', name='packet'),
-    url(r'^packet/(?P<slug>.*)$', 'frontend.views.packet', name='packet'),
-    url(r'^downloads/(?P<slug>.*)$', 'frontend.views.downloads', name='downloads'),
-    url(r'^downapps$', 'frontend.views.downapps', name='downapps'),
-    url(r'^oauth/wechat$', 'frontend.views.wechat'),
+    url(r'^$', 'service.frontend.views.home', name='home'),
+    url(r'^help', 'service.frontend.views.help', name='help'),
+    url(r'^packet$', 'service.frontend.views.packet', name='packet'),
+    url(r'^packet/(?P<slug>.*)$', 'service.frontend.views.packet', name='packet'),
+    url(r'^downloads/(?P<slug>.*)$', 'service.frontend.views.downloads', name='downloads'),
+    url(r'^downapps$', 'service.frontend.views.downapps', name='downapps'),
+    url(r'^oauth/wechat$', 'service.frontend.views.wechat'),
 
-    url(r'^api/', include('restful.urls')),
-
+    url(r'^api/', include('service.backends.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
